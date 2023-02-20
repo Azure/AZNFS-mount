667665b40eaaddb446964a973b56baadb9176b4d
Chandan Agrawal <cagrawal@microsoft.com>
2023-02-20 22:58:29 +0530

    Allow non-super user to mount

--- package.sh
+++ package.sh
@@ -20,11 +20,12 @@ sed -i -e "s/Version: x.y.z/Version: ${BUILD_NUMBER}/g" ${STG_DIR}/${pkg_dir}/DE
 
 # Copy other static package file(s).
 mkdir -p ${STG_DIR}/${pkg_dir}/sbin
-cp -avf ${SOURCE_DIR}/src/mount.aznfs ${STG_DIR}/${pkg_dir}/sbin/
 cp -avf ${SOURCE_DIR}/src/aznfswatchdog ${STG_DIR}/${pkg_dir}/sbin/
 
 mkdir -p ${STG_DIR}/${pkg_dir}/${opt_dir}
 cp -avf ${SOURCE_DIR}/lib/common.sh ${STG_DIR}/${pkg_dir}/${opt_dir}/
+cp -avf ${SOURCE_DIR}/src/mount.aznfs.c ${STG_DIR}/${pkg_dir}/${opt_dir}/
+cp -avf ${SOURCE_DIR}/src/mountscript.sh ${STG_DIR}/${pkg_dir}/${opt_dir}/
 
 mkdir -p ${STG_DIR}/${pkg_dir}/${system_dir}
 cp -avf ${SOURCE_DIR}/src/aznfswatchdog.service ${STG_DIR}/${pkg_dir}/${system_dir}
