12801cb2b1c2f372de3b420afd72441c39a20388
Chandan Agrawal <cagrawal@microsoft.com>
2023-02-22 00:30:05 +0530

    Update github pipeline to generate pipeline assets

--- package.sh
+++ package.sh
@@ -3,9 +3,9 @@
 # Exit on error.
 set -e
 
-#STG_DIR, BUILD_NUMBER and SOURCE_DIR will be taken as env var.
+#STG_DIR, RELEASE_NUMBER and SOURCE_DIR will be taken as env var.
 pkg_name="aznfs"
-pkg_dir=${pkg_name}_${BUILD_NUMBER}_amd64
+pkg_dir="${pkg_name}_${RELEASE_NUMBER}_amd64"
 opt_dir="/opt/microsoft/${pkg_name}"
 system_dir="/lib/systemd/system"
 
@@ -17,19 +17,19 @@ cp -avf ${SOURCE_DIR}/packaging/${pkg_name}/* ${STG_DIR}/${pkg_dir}/
 chmod +x ${STG_DIR}/${pkg_dir}/DEBIAN/*
 
 # Insert current build number.
-sed -i -e "s/Version: x.y.z/Version: ${BUILD_NUMBER}/g" ${STG_DIR}/${pkg_dir}/DEBIAN/control
+sed -i -e "s/Version: x.y.z/Version: ${RELEASE_NUMBER}/g" ${STG_DIR}/${pkg_dir}/DEBIAN/control
 
 # Copy other static package file(s).
 mkdir -p ${STG_DIR}/${pkg_dir}/sbin
 cp -avf ${SOURCE_DIR}/src/aznfswatchdog ${STG_DIR}/${pkg_dir}/sbin/
 
-mkdir -p ${STG_DIR}/${pkg_dir}/${opt_dir}
-cp -avf ${SOURCE_DIR}/lib/common.sh ${STG_DIR}/${pkg_dir}/${opt_dir}/
-cp -avf ${SOURCE_DIR}/src/mount.aznfs.c ${STG_DIR}/${pkg_dir}/${opt_dir}/
-cp -avf ${SOURCE_DIR}/src/mountscript.sh ${STG_DIR}/${pkg_dir}/${opt_dir}/
+mkdir -p ${STG_DIR}/${pkg_dir}${opt_dir}
+cp -avf ${SOURCE_DIR}/lib/common.sh ${STG_DIR}/${pkg_dir}${opt_dir}/
+cp -avf ${SOURCE_DIR}/src/mount.aznfs.c ${STG_DIR}/${pkg_dir}${opt_dir}/
+cp -avf ${SOURCE_DIR}/src/mountscript.sh ${STG_DIR}/${pkg_dir}${opt_dir}/
 
-mkdir -p ${STG_DIR}/${pkg_dir}/${system_dir}
-cp -avf ${SOURCE_DIR}/src/aznfswatchdog.service ${STG_DIR}/${pkg_dir}/${system_dir}
+mkdir -p ${STG_DIR}/${pkg_dir}${system_dir}
+cp -avf ${SOURCE_DIR}/src/aznfswatchdog.service ${STG_DIR}/${pkg_dir}${system_dir}
 
 cd $STG_DIR
 
