8e5779d131a9837fe6545cbf0b9ec7a4ae32567d
Chandan Agrawal <cagrawal@microsoft.com>
2023-02-28 14:42:42 +0530

    Generate binaries to distribute

--- package.sh
+++ package.sh
@@ -24,23 +24,34 @@ mkdir -p ${STG_DIR}/${pkg_dir}/sbin
 cp -avf ${SOURCE_DIR}/src/aznfswatchdog ${STG_DIR}/${pkg_dir}/sbin/
 cp -avf ${SOURCE_DIR}/src/mount.aznfs ${STG_DIR}/${pkg_dir}/sbin/
 
-# Create the binary of aznfswatchdog to distribute.
-shc -f ${STG_DIR}/${pkg_dir}/sbin/aznfswatchdog
-rm -r ${STG_DIR}/${pkg_dir}/sbin/aznfswatchdog
-mv -vf ${STG_DIR}/${pkg_dir}/sbin/aznfswatchdog.x ${STG_DIR}/${pkg_dir}/sbin/aznfswatchdog
-rm -r ${STG_DIR}/${pkg_dir}/sbin/aznfswatchdog.x.c
-
-# Create the binary of mount.aznfs to distribute with SETUID enabled.
-shc -S -f ${STG_DIR}/${pkg_dir}/sbin/mount.aznfs
-rm -r ${STG_DIR}/${pkg_dir}/sbin/mount.aznfs
-mv -vf ${STG_DIR}/${pkg_dir}/sbin/mount.aznfs.x ${STG_DIR}/${pkg_dir}/sbin/mount.aznfs
-rm -r ${STG_DIR}/${pkg_dir}/sbin/mount.aznfs.x.c
-
 mkdir -p ${STG_DIR}/${pkg_dir}${opt_dir}
 cp -avf ${SOURCE_DIR}/lib/common.sh ${STG_DIR}/${pkg_dir}${opt_dir}/
 
 mkdir -p ${STG_DIR}/${pkg_dir}${system_dir}
 cp -avf ${SOURCE_DIR}/src/aznfswatchdog.service ${STG_DIR}/${pkg_dir}${system_dir}
+
+#
+# Change path to ${STG_DIR}/${pkg_dir} so that aznfswatchdog and mount.aznfs
+# can correctly source common.sh while generating binaries.
+#
+cd ${STG_DIR}/${pkg_dir}
+
+# Create the binaries of aznfswatchdog and mount.aznfs to distribute.
+shc -f ${STG_DIR}/${pkg_dir}/sbin/aznfswatchdog
+shc -S -f ${STG_DIR}/${pkg_dir}/sbin/mount.aznfs
+
+# Remove the bash scripts since those will not be distributed to client.
+rm -r ${STG_DIR}/${pkg_dir}/sbin/aznfswatchdog
+rm -r ${STG_DIR}/${pkg_dir}/sbin/mount.aznfs
+rm -r ${STG_DIR}/${pkg_dir}${opt_dir}/common.sh
+
+# Rename the binary files to original file name.
+mv -vf ${STG_DIR}/${pkg_dir}/sbin/aznfswatchdog.x ${STG_DIR}/${pkg_dir}/sbin/aznfswatchdog
+mv -vf ${STG_DIR}/${pkg_dir}/sbin/mount.aznfs.x ${STG_DIR}/${pkg_dir}/sbin/mount.aznfs
+
+# Remove .x.c file.
+rm -r ${STG_DIR}/${pkg_dir}/sbin/aznfswatchdog.x.c
+rm -r ${STG_DIR}/${pkg_dir}/sbin/mount.aznfs.x.c
 
 cd $STG_DIR
 
