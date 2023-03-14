30d6d816cdf936072bdddc20c64943dea399ce73
Chandan Agrawal <cagrawal@microsoft.com>
2023-03-14 10:59:24 +0530

    Major Update

--- scripts/aznfs_install.sh
+++ scripts/aznfs_install.sh
@@ -1,100 +1,253 @@
 #!/bin/bash
-#
-# Copyright (c) Microsoft Corporation.
-#
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 
 RELEASE_NUMBER=x.y.z
+distro_id=
+distro_release=
+distro_release_major=
+apt_update_done=false
+install_cmd=
+yum="yum"
 apt=0
 zypper=0
-debian=0
-yum="yum"
-           
+
+RED="\e[2;31m"
+GREEN="\e[2;32m"
+YELLOW="\e[2;33m"
+NORMAL="\e[0m"
+
+#
+# Core logging function.
+#
+_log()
+{
+    echoarg=""
+
+    # We only support -n argument to echo.
+    if [ "$1" == "-n" ]; then
+        echoarg="-n"
+        shift
+    fi
+
+    color=$1
+    msg=$2
+
+    echo $echoarg -e "${color}${msg}${NORMAL}"
+}
+
+#
+# Plain echo.
+#
+pecho()
+{
+    echoarg=""
+    color=$NORMAL
+    if [ "$1" == "-n" ]; then
+        echoarg="-n"
+        shift
+    fi
+    _log $echoarg $color "${*}"
+}
+
+#
+# Success echo.
+#
+secho()
+{
+    echoarg=""
+    color=$GREEN
+    if [ "$1" == "-n" ]; then
+        echoarg="-n"
+        shift
+    fi
+    _log $echoarg $color "${*}"
+}
+
+#
+# Warning echo.
+#
+wecho()
+{
+    echoarg=""
+    color=$YELLOW
+    if [ "$1" == "-n" ]; then
+        echoarg="-n"
+        shift
+    fi
+    _log $echoarg $color "${*}"
+}
+
+#
+# Error echo.
+#
+eecho()
+{
+    echoarg=""
+    color=$RED
+    if [ "$1" == "-n" ]; then
+        echoarg="-n"
+        shift
+    fi
+    _log $echoarg $color "${*}"
+}
+
 use_dnf_or_yum() 
 {
     yum="yum"
     if command -v dnf &> /dev/null; then
         yum="dnf"
-        localinstall=0
-        echo "Using 'dnf' instead of 'yum'"
+        pecho "Using 'dnf' instead of 'yum'"
     fi
 }
 
+#
+# This returns distro id in a canonical form, that rest of the code understands.
+# We only use lowercase single word names for distro names:
+# ubuntu, centos, redhat, sles.
+#
+canonicalize_distro_id()
+{
+    local distro_lower=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+
+    # Use sles for SUSE/SLES.
+    if [ "$distro_lower" == "suse" ]; then
+        distro_lower="sles"
+    fi
+
+    echo "$distro_lower"
+}
+
+#
+# Install the package appropriately as per the current distro.
+# If distro_id is already detected it uses that else it tries to guess
+# the distro.
+#
+ensure_pkg()
+{
+    local pkg="$1"
+    local distro="$distro_id"
+
+    #
+    # If distro not detected yet, try to make a fair guess, just for the
+    # purpose of package installation.
+    #
+    if [ -z "$distro" ]; then
+        if which zypper 2>/dev/null; then
+            distro="sles"
+        elif which apt 2>/dev/null; then
+            distro="ubuntu"
+        elif which yum 2>/dev/null; then
+            distro="centos"
+        else
+            echo
+            eecho "[FATAL] Could not detect/guess distro!"
+            exit 1
+        fi
+    fi
+
+    if [ "$distro" == "ubuntu" ]; then
+        if ! $apt_update_done; then
+            sudo apt update
+            if [ $? -ne 0 ]; then
+                echo
+                eecho "\"apt update\" failed"
+                eecho "Please make sure \"apt update\" runs successfully and then try again!"
+                exit 1
+                echo
+            fi
+            # Need to run apt update only once.
+            apt_update_done=true
+        fi
+        apt=1
+        sudo apt install -y $pkg
+    elif [ "$distro" == "centos" -o "$distro" == "rocky" -o "$distro" == "rhel" ]; then
+        # lsb_release package is called redhat-lsb-core in redhat/centos.
+        if [ "$pkg" == "lsb-release" ]; then
+            pkg="redhat-lsb-core"
+        fi
+        use_dnf_or_yum
+        sudo $yum install -y $pkg
+    elif [ "$distro" == "sles" ]; then
+        zypper=1
+        sudo zypper install -y $pkg
+    fi
+}
+
+#
+# Once this function return successfully, lsb_release would be installed.
+#
+ensure_lsb_release()
+{
+    lsb_release=$(which lsb_release 2>/dev/null)
+    if [ $? -eq 0 ]; then
+        if [ -x "$lsb_release" ]; then
+            return
+        fi
+
+        echo
+        eecho "[FATAL] $lsb_release is not executable!"
+        exit 1
+    fi
+
+    ensure_pkg "lsb-release"
+}
+
 if [ $RELEASE_NUMBER == "x.y.z" ]; then
-    echo "This script is directly downloaded from the github source code."
-    echo "Please download the aznfs_install.sh from https://github.com/Azure/BlobNFS-mount/releases/latest"
-    echo "If the problem persists, contact Microsoft support."
+    eecho "This script is directly downloaded from the github source code."
+    eecho "Please download the aznfs_install.sh from https://github.com/Azure/BlobNFS-mount/releases/latest"
+    eecho "If the problem persists, contact Microsoft support."
     exit 1
 fi
 
-# Detect OS and Version
+#
+# Detect OS and Version.
+#
 __m=$(uname -m 2>/dev/null) || __m=unknown
 __s=$(uname -s 2>/dev/null) || __s=unknown
 
-distro=
-distro_version=
+#
+# Try to detect the distro in a resilient manner and set distro_id and
+# distro_release global variables.
+#
 case "${__m}:${__s}" in
     "x86_64:Linux")
+
         if [ -f /etc/centos-release ]; then
-            echo "Retrieving distro info from /etc/centos-release..."
-            distro=$(awk -F" " '{ print $1 }' /etc/centos-release)
-            distro_version=$(awk -F" " '{ print $4 }' /etc/centos-release)
-        elif [ -f /etc/os-release ]; then
-            echo "Retrieving distro info from /etc/os-release..."
-            distro=$(grep ^NAME /etc/os-release | awk -F"=" '{ print $2 }' | tr -d '"')
-            distro_version=$(grep VERSION_ID /etc/os-release | awk -F"=" '{ print $2 }' | tr -d '"')
-        elif which lsb_release 2>/dev/null; then
-            echo "Retrieving distro info from lsb_release command..."
-            distro=$(lsb_release -i | awk -F":" '{ print $2 }')
-            distro_version=$(lsb_release -r | awk -F":" '{ print $2 }')
-        else
-            echo "Unknown linux distro."
-            exit 1
+            pecho "Retrieving distro info from /etc/centos-release..."
+            distro_id="centos"
+            distro_release=$(awk -F' ' '{ print $4 }' /etc/centos-release)
+            distro_release_major=$(echo $distro_release | awk -F. '{print $1}')
+            return
         fi
+
+        if [ -f /etc/os-release ]; then
+            pecho "Retrieving distro_id info from /etc/os-release..."
+            distro_id=$(grep "^ID=" /etc/os-release | awk -F= '{print $2}' | tr -d '"')
+            distro_id=$(canonicalize_distro_id $distro_id)
+        fi
+
+        # We need the exact release for downloading the correct kernel for centos.
+        pecho "Retrieving distro_release info from lsb_release command..."
+        ensure_lsb_release
+        distro_release=$(lsb_release -r | cut -d: -f2)
+        distro_release_major=$(echo $distro_release | awk -F. '{print $1}')
         ;;
     *)
-        echo "Unsupported platform: ${__m}:${__s}."
+        eecho "[FATAL] Unsupported platform: ${__m}:${__s}."
         exit 1
         ;;
 esac
 
-case "${distro}" in
-    *entOS*)
-        use_dnf_or_yum
-        sudo -E ${yum} -y install wget
-        ;;
+ensure_pkg "wget"
 
-    *SLES*)
-        zypper=1
-        sudo -E zypper install -y wget
-        ;;
-
-    *buntu*)
-        apt=1
-        sudo -E apt update
-        sudo -E apt install -y wget
-        ;;
-
-    *ocky*)
-        use_dnf_or_yum
-        sudo -E ${yum} -y install wget
-        ;;
-
-    *Redhat*)
-        use_dnf_or_yum
-        sudo -E ${yum} -y install wget
-        ;;
-
-    *)
-        echo "[FATAL] Unsupported Linux distribution: ${distro}:${distro_version}."
-        exit 1
-        ;;
-esac
-
-install_cmd=
 if [ $apt -eq 1 ]; then
     install_cmd="apt"
     wget https://github.com/Azure/BlobNFS-mount/releases/download/${RELEASE_NUMBER}/aznfs_${RELEASE_NUMBER}_amd64.deb -P /tmp
     sudo apt install /tmp/aznfs_${RELEASE_NUMBER}_amd64.deb
+    install_error=$?
     rm /tmp/aznfs_${RELEASE_NUMBER}_amd64.deb
 elif [ $zypper -eq 1 ]; then
     install_cmd="zypper"
@@ -104,9 +257,8 @@ else
     # Does not support CentOS for now.
 fi
 
-install_exit_code=$?
-if [ $install_exit_code -ne 0 ]; then
-    echo "[FATAL] Error installing aznfs (exit code: $install_exit_code). See '$install_cmd' command logs for more information."
+if [ $install_error -ne 0 ]; then
+    eecho "[FATAL] Error installing aznfs (Error: $install_error). See '$install_cmd' command logs for more information."
 fi
 
-echo "Latest version of aznfs is installed."
+secho "Latest version of aznfs is installed."
