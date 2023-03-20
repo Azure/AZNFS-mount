f6e83a881e0e79cf619c83515f66ad19cb7ab963
Chandan Agrawal <cagrawal@microsoft.com>
2023-03-20 17:45:34 +0530

    Update

--- package.sh
+++ package.sh
@@ -14,33 +14,68 @@ pkg_dir="${pkg_name}_${RELEASE_NUMBER}_amd64"
 opt_dir="/opt/microsoft/${pkg_name}"
 system_dir="/lib/systemd/system"
 
-# Create the directory to hold the package control and data files.
-mkdir -p ${STG_DIR}/${pkg_dir}
+# Create the directory to hold the package control and data files for deb package.
+mkdir -p ${STG_DIR}/deb/${pkg_dir}/DEBIAN
 
 # Copy the debian control file(s) and maintainer scripts.
-cp -avf ${SOURCE_DIR}/packaging/${pkg_name}/* ${STG_DIR}/${pkg_dir}/
-chmod +x ${STG_DIR}/${pkg_dir}/DEBIAN/*
+cp -avf ${SOURCE_DIR}/packaging/${pkg_name}/DEBIAN/* ${STG_DIR}/deb/${pkg_dir}/DEBIAN/
+chmod +x ${STG_DIR}/deb/${pkg_dir}/DEBIAN/*
 
 # Insert current release number.
-sed -i -e "s/Version: x.y.z/Version: ${RELEASE_NUMBER}/g" ${STG_DIR}/${pkg_dir}/DEBIAN/control
+sed -i -e "s/Version: x.y.z/Version: ${RELEASE_NUMBER}/g" ${STG_DIR}/deb/${pkg_dir}/DEBIAN/control
 sed -i -e "s/RELEASE_NUMBER=x.y.z/RELEASE_NUMBER=${RELEASE_NUMBER}/g" ${SOURCE_DIR}/scripts/aznfs_install.sh
 
 # Copy other static package file(s).
-mkdir -p ${STG_DIR}/${pkg_dir}/usr/sbin
-cp -avf ${SOURCE_DIR}/src/aznfswatchdog ${STG_DIR}/${pkg_dir}/usr/sbin/
+mkdir -p ${STG_DIR}/deb/${pkg_dir}/usr/sbin
+cp -avf ${SOURCE_DIR}/src/aznfswatchdog ${STG_DIR}/deb/${pkg_dir}/usr/sbin/
 
-# Compile mount.aznfs.c and put the executable into ${STG_DIR}/${pkg_dir}/sbin.
-mkdir -p ${STG_DIR}/${pkg_dir}/sbin
-gcc -static ${SOURCE_DIR}/src/mount.aznfs.c -o ${STG_DIR}/${pkg_dir}/sbin/mount.aznfs
+# Compile mount.aznfs.c and put the executable into ${STG_DIR}/deb/${pkg_dir}/sbin.
+mkdir -p ${STG_DIR}/deb/${pkg_dir}/sbin
+gcc -static ${SOURCE_DIR}/src/mount.aznfs.c -o ${STG_DIR}/deb/${pkg_dir}/sbin/mount.aznfs
 
-mkdir -p ${STG_DIR}/${pkg_dir}${opt_dir}
-cp -avf ${SOURCE_DIR}/lib/common.sh ${STG_DIR}/${pkg_dir}${opt_dir}/
-cp -avf ${SOURCE_DIR}/src/mountscript.sh ${STG_DIR}/${pkg_dir}${opt_dir}/
+mkdir -p ${STG_DIR}/deb/${pkg_dir}${opt_dir}
+cp -avf ${SOURCE_DIR}/lib/common.sh ${STG_DIR}/deb/${pkg_dir}${opt_dir}/
+cp -avf ${SOURCE_DIR}/src/mountscript.sh ${STG_DIR}/deb/${pkg_dir}${opt_dir}/
 
-mkdir -p ${STG_DIR}/${pkg_dir}${system_dir}
-cp -avf ${SOURCE_DIR}/src/aznfswatchdog.service ${STG_DIR}/${pkg_dir}${system_dir}
+mkdir -p ${STG_DIR}/deb/${pkg_dir}${system_dir}
+cp -avf ${SOURCE_DIR}/src/aznfswatchdog.service ${STG_DIR}/deb/${pkg_dir}${system_dir}
 
-cd $STG_DIR
+# Create the deb package.
+dpkg-deb --root-owner-group --build $STG_DIR/deb/$pkg_dir
 
-# Create the package.
-dpkg-deb --root-owner-group --build $pkg_dir
+# Create the directory to hold the package control and data files for RPM package.
+mkdir -p ${STG_DIR}/rpm/${pkg_dir}/tmp
+
+# Copy other static package file(s).
+mkdir -p ${STG_DIR}/rpm/${pkg_dir}/tmp/usr/sbin
+cp -avf ${SOURCE_DIR}/src/aznfswatchdog ${STG_DIR}/rpm/${pkg_dir}/tmp/usr/sbin/
+
+# Compile mount.aznfs.c and put the executable into ${STG_DIR}/rpm/${pkg_dir}/tmp/sbin.
+mkdir -p ${STG_DIR}/rpm/${pkg_dir}/tmp/sbin
+gcc -static ${SOURCE_DIR}/src/mount.aznfs.c -o ${STG_DIR}/rpm/${pkg_dir}/tmp/sbin/mount.aznfs
+
+mkdir -p ${STG_DIR}/rpm/${pkg_dir}/tmp${opt_dir}
+cp -avf ${SOURCE_DIR}/lib/common.sh ${STG_DIR}/rpm/${pkg_dir}/tmp${opt_dir}/
+cp -avf ${SOURCE_DIR}/src/mountscript.sh ${STG_DIR}/rpm/${pkg_dir}/tmp${opt_dir}/
+
+mkdir -p ${STG_DIR}/rpm/${pkg_dir}/tmp${system_dir}
+cp -avf ${SOURCE_DIR}/src/aznfswatchdog.service ${STG_DIR}/rpm/${pkg_dir}/tmp${system_dir}
+
+# Create the archive for the package and put it into ${STG_DIR}/rpm/${pkg_dir}/SOURCES.
+mkdir -p ${STG_DIR}/rpm/${pkg_dir}/SOURCES
+cd ${STG_DIR}/rpm/${pkg_dir}/tmp
+tar -cvzf ${pkg_name}_${RELEASE_NUMBER}.tar.gz ${STG_DIR}/rpm/${pkg_dir}/tmp
+mv ${STG_DIR}/rpm/${pkg_dir}/tmp/${pkg_name}_${RELEASE_NUMBER}.tar.gz ${STG_DIR}/rpm/${pkg_dir}/SOURCES
+ 
+# Copy the RPM .spec file.
+cp -avf ${SOURCE_DIR}/packaging/${pkg_name}/RPM/* ${STG_DIR}/rpm/${pkg_dir}/
+
+# Insert current release number.
+sed -i -e "s/Version: x.y.z/Version: ${RELEASE_NUMBER}/g" ${STG_DIR}/rpm/${pkg_dir}/SPECS/aznfs.spec
+
+mkdir -p ${STG_DIR}/rpm/${pkg_dir}/BUILD
+mkdir -p ${STG_DIR}/rpm/${pkg_dir}/RPMS
+mkdir -p ${STG_DIR}/rpm/${pkg_dir}/SRPMS
+
+# Create the rpm package.
+rpmbuild -bb ${STG_DIR}/rpm/${pkg_dir}/SPECS/aznfs.spec
