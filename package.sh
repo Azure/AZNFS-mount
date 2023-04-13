c299f49dd1ee05506eb9c91874802976fd94770c
Chandan Agrawal <cagrawal@microsoft.com>
2023-04-14 00:07:51 +0530

    Updates

--- package.sh
+++ package.sh
@@ -13,6 +13,14 @@ pkg_name="aznfs"
 pkg_dir="${pkg_name}_${RELEASE_NUMBER}_amd64"
 opt_dir="/opt/microsoft/${pkg_name}"
 system_dir="/lib/systemd/system"
+rpmbuild_dir="/root/rpmbuild"
+rpm_buildroot_dir="${rpmbuild_dir}/BUILDROOT"
+rpm_source_dir="${rpmbuild_dir}/SOURCE"
+
+# Insert releaes number to aznfs_install.sh
+sed -i -e "s/RELEASE_NUMBER=x.y.z/RELEASE_NUMBER=${RELEASE_NUMBER}/g" ${SOURCE_DIR}/scripts/aznfs_install.sh
+
+#Genrate .deb package.
 
 # Create the directory to hold the package control and data files for deb package.
 mkdir -p ${STG_DIR}/deb/${pkg_dir}/DEBIAN
@@ -23,7 +31,6 @@ chmod +x ${STG_DIR}/deb/${pkg_dir}/DEBIAN/*
 
 # Insert current release number.
 sed -i -e "s/Version: x.y.z/Version: ${RELEASE_NUMBER}/g" ${STG_DIR}/deb/${pkg_dir}/DEBIAN/control
-sed -i -e "s/RELEASE_NUMBER=x.y.z/RELEASE_NUMBER=${RELEASE_NUMBER}/g" ${SOURCE_DIR}/scripts/aznfs_install.sh
 
 # Copy other static package file(s).
 mkdir -p ${STG_DIR}/deb/${pkg_dir}/usr/sbin
@@ -43,39 +50,31 @@ cp -avf ${SOURCE_DIR}/src/aznfswatchdog.service ${STG_DIR}/deb/${pkg_dir}${syste
 # Create the deb package.
 dpkg-deb --root-owner-group --build $STG_DIR/deb/$pkg_dir
 
+#Generate .rpm package.
+
 # Create the directory to hold the package control and data files for RPM package.
-mkdir -p ${STG_DIR}/rpm/${pkg_dir}/tmp
+mkdir -p ${STG_DIR}/rpm/tmp${rpm_buildroot_dir}/${pkg_dir}
 
 # Copy other static package file(s).
-mkdir -p ${STG_DIR}/rpm/${pkg_dir}/tmp/usr/sbin
-cp -avf ${SOURCE_DIR}/src/aznfswatchdog ${STG_DIR}/rpm/${pkg_dir}/tmp/usr/sbin/
+mkdir -p ${STG_DIR}/rpm/tmp${rpm_buildroot_dir}/${pkg_dir}/usr/sbin
+cp -avf ${SOURCE_DIR}/src/aznfswatchdog ${STG_DIR}/rpm/tmp${rpm_buildroot_dir}/${pkg_dir}/usr/sbin/
 
 # Compile mount.aznfs.c and put the executable into ${STG_DIR}/rpm/${pkg_dir}/tmp/sbin.
-mkdir -p ${STG_DIR}/rpm/${pkg_dir}/tmp/sbin
-gcc -static ${SOURCE_DIR}/src/mount.aznfs.c -o ${STG_DIR}/rpm/${pkg_dir}/tmp/sbin/mount.aznfs
+mkdir -p ${STG_DIR}/rpm/tmp${rpm_buildroot_dir}/${pkg_dir}/sbin
+gcc -static ${SOURCE_DIR}/src/mount.aznfs.c -o ${STG_DIR}/rpm/tmp${rpm_buildroot_dir}/${pkg_dir}/sbin/mount.aznfs
 
-mkdir -p ${STG_DIR}/rpm/${pkg_dir}/tmp${opt_dir}
-cp -avf ${SOURCE_DIR}/lib/common.sh ${STG_DIR}/rpm/${pkg_dir}/tmp${opt_dir}/
-cp -avf ${SOURCE_DIR}/src/mountscript.sh ${STG_DIR}/rpm/${pkg_dir}/tmp${opt_dir}/
+mkdir -p ${STG_DIR}/rpm/tmp${rpm_buildroot_dir}/${pkg_dir}${opt_dir}
+cp -avf ${SOURCE_DIR}/lib/common.sh ${STG_DIR}/rpm/tmp${rpm_buildroot_dir}/${pkg_dir}${opt_dir}/
+cp -avf ${SOURCE_DIR}/src/mountscript.sh ${STG_DIR}/rpm/tmp${rpm_buildroot_dir}/${pkg_dir}${opt_dir}/
 
-mkdir -p ${STG_DIR}/rpm/${pkg_dir}/tmp${system_dir}
-cp -avf ${SOURCE_DIR}/src/aznfswatchdog.service ${STG_DIR}/rpm/${pkg_dir}/tmp${system_dir}
+mkdir -p ${STG_DIR}/rpm/tmp${rpm_buildroot_dir}/${pkg_dir}${system_dir}
+cp -avf ${SOURCE_DIR}/src/aznfswatchdog.service ${STG_DIR}/rpm/tmp${rpm_buildroot_dir}/${pkg_dir}${system_dir}
 
-# Create the archive for the package and put it into ${STG_DIR}/rpm/${pkg_dir}/SOURCES.
-mkdir -p ${STG_DIR}/rpm/${pkg_dir}/SOURCES
-cd ${STG_DIR}/rpm/${pkg_dir}/tmp
-tar -cvzf ${pkg_name}_${RELEASE_NUMBER}.tar.gz ${STG_DIR}/rpm/${pkg_dir}/tmp
-mv ${STG_DIR}/rpm/${pkg_dir}/tmp/${pkg_name}_${RELEASE_NUMBER}.tar.gz ${STG_DIR}/rpm/${pkg_dir}/SOURCES
- 
-# Copy the RPM .spec file.
-cp -avf ${SOURCE_DIR}/packaging/${pkg_name}/RPM/* ${STG_DIR}/rpm/${pkg_dir}/
+# Create the archive for the package.
+tar -cvzf ${pkg_dir}.tar.gz -C ${STG_DIR}/rpm/tmp root
 
 # Insert current release number.
 sed -i -e "s/Version: x.y.z/Version: ${RELEASE_NUMBER}/g" ${STG_DIR}/rpm/${pkg_dir}/SPECS/aznfs.spec
 
-mkdir -p ${STG_DIR}/rpm/${pkg_dir}/BUILD
-mkdir -p ${STG_DIR}/rpm/${pkg_dir}/RPMS
-mkdir -p ${STG_DIR}/rpm/${pkg_dir}/SRPMS
-
 # Create the rpm package.
-rpmbuild -bb ${STG_DIR}/rpm/${pkg_dir}/SPECS/aznfs.spec
+rpmbuild --define "_topdir ${STG_DIR}/rpm${rpmbuild_dir}" -v -bb ${SOURCE_DIR}/RPM/aznfs.spec
--- packaging/aznfs/RPM/aznfs.spec
+++ packaging/aznfs/RPM/aznfs.spec
@@ -3,19 +3,22 @@ Version: x.y.z
 Release: 1
 Summary: Mount helper program for correctly handling endpoint IP address changes for Azure Blob NFS mounts
 License: MIT
-URL: https://github.com/Azure/BlobNFS-mount/blob/main/README.md
+URL: https://github.com/Azure/AZNFS-mount/blob/main/README.md
 Source0: https://github.com/Azure/BlobNFS-mount/archive/refs/tags/%{Version}.tar.gz
-Requires: conntrack, iptables, bind9-host, iproute2, util-linux, nfs-common
-BuildArch: amd64
+Requires: conntrack, iptables, bind-utils, iproute, util-linux, nfs-utils, netcat
 
 %description
 Mount helper program for correctly handling endpoint IP address changes for Azure Blob NFS mounts
 
+%prep
+cp ${STG_DIR}/aznfs_${RELEASE_NUMBER}_amd64.tar.gz ${STG_DIR}/rpm/root/rpmbuild/SOURCE/
+tar -xzvf ${STG_DIR}/rpm/root/rpmbuild/SOURCE/aznfs_${RELEASE_NUMBER}_amd64.tar.gz -C ${STG_DIR}/rpm/
+
 %files
 %{_sbindir}/aznfswatchdog
 /sbin/mount.aznfs
-/opt/microsoft/%{Name}/common.sh
-/opt/microsoft/%{Name}/mountscript.sh
+/opt/microsoft/aznfs/common.sh
+/opt/microsoft/aznfs/mountscript.sh
 /lib/systemd/system/aznfswatchdog.service
 
 %pre
@@ -42,13 +45,27 @@ chmod 0644 /opt/microsoft/aznfs/common.sh
 # Set suid bit for mount.aznfs to allow mount for non-super user.
 chmod 4755 /sbin/mount.aznfs
 
+if [ ! -s /opt/microsoft/aznfs/randbytes ]; then
+	dd if=/dev/urandom of=/opt/microsoft/aznfs/randbytes bs=256 count=1
+fi
+if [ ! -s /opt/microsoft/aznfs/randbytes ]; then
+	uuidgen > /opt/microsoft/aznfs/randbytes
+fi
+if [ ! -s /opt/microsoft/aznfs/randbytes ]; then
+	date | md5sum | awk '{print $1}' > /opt/microsoft/aznfs/randbytes
+fi
+if [ ! -s /opt/microsoft/aznfs/randbytes ]; then
+	date > /opt/microsoft/aznfs/randbytes
+fi
+chattr +i /opt/microsoft/aznfs/randbytes
+
 # Start aznfswatchdog service.
 systemctl daemon-reload
 systemctl enable aznfswatchdog
 systemctl start aznfswatchdog
 
 %preun
-# In case of perge/remove.
+# In case of purge/remove.
 if [ $1 == 0 ];then
 	# Verify if any existing mounts are there, warn the user about this.
 	existing_mounts=$(cat /opt/microsoft/aznfs/mountmap 2>/dev/null | wc -l)
@@ -60,10 +77,15 @@ if [ $1 == 0 ];then
 			exit 1
 		fi
 	fi
+
+	# Stop aznfswatchdog in case of removing the package.
+	systemctl stop aznfswatchdog
+	systemctl disable aznfswatchdog
+	echo "Stopped aznfswatchdog service."
 fi
 
 %postun
-# In case of perge/remove.
+# In case of purge/remove.
 if [ $1 == 0 ];then
    chattr -if /opt/microsoft/aznfs/mountmap
 	rm -rf /opt/microsoft/aznfs
