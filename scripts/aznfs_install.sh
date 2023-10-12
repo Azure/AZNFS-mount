25970e35b7c6404d7c31407be827093ad1444b08
palash-vij <palashvij@microsoft.com>
2023-10-13 00:47:14 +0530

    release 212

--- scripts/aznfs_install.sh
+++ scripts/aznfs_install.sh
@@ -393,7 +393,7 @@ if [ "$RUN_MODE" == "auto-update" ]; then
     #     eecho "**************************************************************"
     #     exit 1
     # fi
-    RELEASE_NUMBER="0.1.212"
+    RELEASE_NUMBER="0.1.213"
 fi
 
 if [ $apt -eq 1 ]; then
