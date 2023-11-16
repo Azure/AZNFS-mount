07a0997b1b8182f1fdc6950ecb5fe3b2d79acc40
palash-vij <palashvij@microsoft.com>
2023-11-16 12:51:48 +0530

    adding logic deleting unreplied conntrack

--- lib/common.sh
+++ lib/common.sh
@@ -494,9 +494,12 @@ ensure_iptable_entry_not_exist()
         fi
 
         # Ignore status of conntrack because entry may not exist (timed out).
-        output=$(conntrack -D conntrack -p tcp -d "$1" -r "$2" 2>&1)
+        output=$(conntrack -D conntrack -p tcp -d "$1" 2>&1)
         if [ $? -ne 0 ]; then
             vecho "$output"
+
+            vecho "conntrack output = $(conntrack -L)"
+            vecho "iptables output = $(iptables -t nat -L -v)"
         fi
     fi
 }
