03a43e02b924eced469cfe1d6b4578fe86b2d03c
palashvij-msft <palashvij@microsoft.com>
2024-01-23 11:44:37 +0530

    update

--- .github/actions/aznfs-prep-machine/machineSetup.sh
+++ .github/actions/aznfs-prep-machine/machineSetup.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+install_dependencies() {
+    local distro="$1"
+
+    if [ "$distro" == "ubuntu" ]; then
+        sudo apt-get update
+        sudo apt-get install -y build-essential
+    elif [ "$distro" == "centos" -o "$distro" == "rocky" -o "$distro" == "rhel" ]; then
+        sudo yum groupinstall -y "Development Tools"
+    fi
+}
+
+untar_unix_test_suite() {
+    mkdir -p /lib
+    tar -xf "$GITHUB_WORKSPACE/UnixTestSuite.tar" -C /lib
+}
+
 runs_on="$1"  # The value passed from the workflow.
 
-if [ "$runs_on" == "ubuntu" ]; then
-    sudo apt-get update
-    sudo apt-get install -y build-essential
-elif [ "$runs_on" == "centos" -o "$runs_on" == "rocky" -o "$runs_on" == "rhel" ]; then
-    sudo yum groupinstall -y "Development Tools"
-fi
+install_dependencies "$runs_on"
+untar_unix_test_suite
