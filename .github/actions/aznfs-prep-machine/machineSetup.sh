a3eec1278f385b31d3951a8d36be16974d18c95f
palashvij-msft <palashvij@microsoft.com>
2024-02-02 12:27:39 +0530

    update for cleanup script

--- .github/actions/aznfs-prep-machine/machineSetup.sh
+++ .github/actions/aznfs-prep-machine/machineSetup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+MOUNTMAP="/opt/microsoft/aznfs/data/mountmap"
+
 install_dependencies() 
 {
     local runner="$1"
@@ -39,7 +41,87 @@ untar_unix_test_suite()
 
 }
 
+umount_all()
+{
+    echo "unmounting all nfs shares (if any)"
+
+    nfsstat_output=$(nfsstat -m)
+    if [ -n "$nfsstat_output" ]; then
+        echo "nfsstat output: $nfsstat_output"
+        sudo umount -af -t nfs
+
+        # Check the exit status
+        if [ $? -eq 0 ]; then
+            echo "Unmount successful."
+        else
+            echo "Unmount failed. Check for errors."
+        fi
+    fi
+}
+
+remove_aznfs()
+{
+    local runner="$1"
+
+    # Check if MOUNTMAP is empty.
+    existing_mounts=$(cat "$MOUNTMAP" 2>/dev/null | egrep '^\S+' | wc -l)
+
+    timeout=300  # Maximum wait-time (MOUNTMAP_INACTIVITY_SECONDS).
+    start_time=$(date +%s)
+
+    # Keep waiting if existing_mounts is not equal to 0.
+    while [ "$existing_mounts" -ne 0 ]; do
+        current_time=$(date +%s)
+        elapsed_time=$((current_time - start_time))
+
+        if [ "$elapsed_time" -ge "$timeout" ]; then
+            echo "[ERROR] Timed out waiting for MOUNTMAP to become empty."
+            exit 1
+        fi
+
+        echo "Waiting for MOUNTMAP to become empty..."
+        sleep 30
+
+        # Update existing_mounts after each iteration.
+        existing_mounts=$(cat "$MOUNTMAP" 2>/dev/null | egrep '^\S+' | wc -l)
+    done
+
+    if [ "$runner" == "self-hosted-ubuntu18" -o "$runner" == "self-hosted-ubuntu20" -o "$runner" == "self-hosted-ubuntu22" ]; then
+        sudo apt purge -y aznfs
+    elif [ "$runner" == "self-hosted-centos7" -o "$runner" == "self-hosted-centos8" -o "$runner" == "self-hosted-redhat7" -o "$runner" == "self-hosted-redhat8" -o "$runner" == "self-hosted-redhat9" -o "$runner" == "self-hosted-rocky8" -o "$runner" == "self-hosted-rocky9" ]; then
+        sudo yum remove -y aznfs
+    elif [ "$runner" == "self-hosted-suse15" ]; then
+        sudo zypper remove -y aznfs_sles
+    fi
+
+    remove_error=$?
+    if [ $remove_error -ne 0 ]; then
+        echo "[ERROR] Installing removing aznfs for $runner machine failed!"
+        exit 1
+    fi
+}
+
+check_if_aznfs_installed_already()
+{
+    local runner="$1"
+
+    if [ "$runner" == "self-hosted-ubuntu18" -o "$runner" == "self-hosted-ubuntu20" -o "$runner" == "self-hosted-ubuntu22" ]; then
+        current_version=$(apt-cache show aznfs 2>/dev/null | grep "^Version" | tr -d " " | cut -d ':' -f2)
+    elif [ "$runner" == "self-hosted-centos7" -o "$runner" == "self-hosted-centos8" -o "$runner" == "self-hosted-redhat7" -o "$runner" == "self-hosted-redhat8" -o "$runner" == "self-hosted-redhat9" -o "$runner" == "self-hosted-rocky8" -o "$runner" == "self-hosted-rocky9" ]; then
+        current_version=$(yum info aznfs 2>/dev/null | grep "^Version" | tr -d " " | cut -d ':' -f2)
+    elif [ "$runner" == "self-hosted-suse15" ]; then
+        current_version=$(zypper info aznfs_sles 2>/dev/null | grep "^Version" | tr -d " " | cut -d ':' -f2 | cut -d '-' -f1)
+    fi
+
+    if [ -n "$current_version" ]; then
+        remove_aznfs "$runner"
+        echo "Successfully removed aznfs $current_version from $runner"
+    fi   
+}
+
 runs_on="$1"  # The value passed from the workflow.
 
 install_dependencies "$runs_on"
 untar_unix_test_suite
+umount_all
+check_if_aznfs_installed_already "$runs_on"
