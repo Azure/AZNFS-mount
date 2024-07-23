76033246f25e0080ec296f549ff0b868b4227d5c
palashvij-msft <palashvij@microsoft.com>
2024-07-23 17:49:49 +0530

    addressing PR comments and updating changelog

--- src/nfsv3mountscript.sh
+++ src/nfsv3mountscript.sh
@@ -231,9 +231,17 @@ fix_dirty_bytes_config()
 }
 
 # Function to extract minor number from combined device ID.
-get_minor() {
+get_minor()
+{
     local dev_id=$1
     echo $(( (dev_id & 0xff) | ((dev_id >> 12) & ~0xff) ))
+}
+
+# Function to extract major number from combined device ID.
+get_major()
+{
+    local dev_id=$1
+    echo $(( ((dev_id >> 8) & 0xfff) | ((dev_id >> 32) & ~0xfff) ))
 }
 
 #
@@ -248,9 +256,10 @@ fix_read_ahead_config()
         return
     fi
 
-    minor=$(get_minor $block_device_id)
     # Path to the read_ahead_kb file.
-    read_ahead_path="/sys/class/bdi/0:$minor/read_ahead_kb"
+    major=$(get_major $block_device_id)
+    minor=$(get_minor $block_device_id)
+    read_ahead_path="/sys/class/bdi/$major:$minor/read_ahead_kb"
     if [ ! -e "$read_ahead_path" ]; then
         wecho "The path $read_ahead_path does not exist. Cannot set read ahead."
         return
