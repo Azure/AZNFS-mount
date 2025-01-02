600b815f0094051e8e3a4d6f6e496fdeda6f15fa
pragyagandhi <pragyagandhi22@gmail.com>
2025-01-02 10:06:27 +0000

    Updated

--- turbonfs/build.sh
+++ turbonfs/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 export VCPKG_ROOT=extern/vcpkg
+mkdir ${VCPKG_ROOT}
+git clone https://github.com/microsoft/vcpkg.git ${VCPKG_ROOT}
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TCMALLOC=OFF -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TCMALLOC=OFF -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake ..
 #cmake -DCMAKE_BUILD_TYPE=Release ..
 #cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_NO_FUSE=ON ..
 make
