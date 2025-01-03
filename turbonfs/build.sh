5596c041f00ce8789d85ba67728e5b503274d8a0
pragyagandhi <pragyagandhi22@gmail.com>
2025-01-03 05:29:39 +0000

    Uopdated

--- turbonfs/build.sh
+++ turbonfs/build.sh
@@ -4,4 +4,4 @@ mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TCMALLOC=OFF -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake ..
 #cmake -DCMAKE_BUILD_TYPE=Release ..
 #cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_NO_FUSE=ON ..
-make VERBOSE=1
+make
