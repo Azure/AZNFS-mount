d770a68f3dcb853cde1810714cb4281d14c9a327
Nagendra Tomar <natomar@microsoft.com>
2025-01-04 01:10:14 +0000

    Update build.sh to be more resilient, now with the new vcpkg addition to the build
    Also, now it takes a buildtype parameter

--- turbonfs/build.sh
+++ turbonfs/build.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
+
+if [ $# -ne 1 ] || [ "$1" != "Release" -a "$1" != "Debug" ]; then
+    echo "Usage: ./build.sh <Release|Debug>"
+    exit 1
+fi
+
+BUILD_TYPE=$1
+
 export VCPKG_ROOT=extern/vcpkg
-# We need to update the submodules before calling cmake as toolchain build expects it.
+
+# Update (vcpkg) submodules before calling cmake as toolchain build expects it.
 git submodule update --recursive --init
+
+# Cleanup old build directory (before vcpkg changes) if present.
+if [ ! -d build/vcpkg_installed ]; then
+    rm -fr build
+fi
+
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TCMALLOC=OFF -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake ..
-#cmake -DCMAKE_BUILD_TYPE=Release ..
+
+if [ "$BUILD_TYPE" == "Debug" ]; then
+    # tcmalloc doesn't play well with ASAN.
+    TCMALLOC=OFF
+else
+    TCMALLOC=ON
+fi
+
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+      -DENABLE_TCMALLOC=$TCMALLOC \
+      -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake ..
+
 #cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_NO_FUSE=ON ..
+
 make
