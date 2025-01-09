0a3960bd3a028f8599d62a29c81702a66737fab9
Nagendra Tomar <natomar@microsoft.com>
2025-01-09 07:08:23 +0000

    Added cmake option ENABLE_INSECURE_AUTH_FOR_DEVTEST

--- turbonfs/build.sh
+++ turbonfs/build.sh
@@ -22,12 +22,18 @@ mkdir -p build && cd build
 if [ "$BUILD_TYPE" == "Debug" ]; then
     # tcmalloc doesn't play well with ASAN.
     TCMALLOC=OFF
+    PARANOID=ON
+    INSECURE_AUTH_FOR_DEVTEST=ON
 else
     TCMALLOC=ON
+    PARANOID=OFF
+    INSECURE_AUTH_FOR_DEVTEST=OFF
 fi
 
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DENABLE_TCMALLOC=$TCMALLOC \
+      -DENABLE_PARANOID=$PARANOID \
+      -DENABLE_INSECURE_AUTH_FOR_DEVTEST=$INSECURE_AUTH_FOR_DEVTEST \
       -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake ..
 
 #cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_NO_FUSE=ON ..
