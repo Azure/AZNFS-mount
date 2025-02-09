dd6589fa1c3ab3a0c72d82fbc5b41169703a153b
palashvij-msft <palashvij@microsoft.com>
2025-02-10 03:29:17 +0530

    update

--- scripts/aznfs_install.sh
+++ scripts/aznfs_install.sh
@@ -428,6 +428,21 @@ case "${__m}:${__s}" in
             exit 1
         fi
         ;;
+    "aarch64:Linux")
+        if [ -f /etc/centos-release ]; then
+            pecho "Retrieving distro info from /etc/centos-release..."
+            distro_id="centos"
+        elif [ -f /etc/os-release ]; then
+            pecho "Retrieving distro info from /etc/os-release..."
+            distro_id=$(grep "^ID=" /etc/os-release | awk -F= '{print $2}' | tr -d '"')
+            distro_id=$(canonicalize_distro_id $distro_id)
+        else
+            eecho "[FATAL] Unknown linux distro, /etc/os-release not found!"
+            pecho "Download .deb/.rpm package based on your distro from 'https://github.com/Azure/AZNFS-mount/releases/latest' or try running install after setting env variable 'AZNFS_FORCE_PACKAGE_MANAGER' to one of 'apt', 'yum', 'dnf', or 'zypper'"
+            pecho "If the problem persists, contact Microsoft support."
+            exit 1
+        fi
+        ;;
     *)
         eecho "[FATAL] Unsupported platform: ${__m}:${__s}."
         exit 1
