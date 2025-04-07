62ae709f79821f7edc8d8710d446d9527a9a4e81
Palash Vij <palashvij@microsoft.com>
2025-04-07 17:18:11 +0530

    online review

--- turbonfs/src/main.cpp
+++ turbonfs/src/main.cpp
@@ -766,10 +766,10 @@ int main(int argc, char *argv[])
      * fuse process, which helps in debugging.
      */
     mount_source = aznfsc_cfg.server + ":" + aznfsc_cfg.export_path +
-                    "[PID=" + std::to_string(::getpid()) +
-                    "][vers=" + std::to_string(AZNFSCLIENT_VERSION_MAJOR) + "." +
-                            std::to_string(AZNFSCLIENT_VERSION_MINOR) + "." +
-                            std::to_string(AZNFSCLIENT_VERSION_PATCH) + "]";
+                    "[PID=" + std::to_string(::getpid()) + "][vers=" +
+                              std::to_string(AZNFSCLIENT_VERSION_MAJOR) + "." +
+                              std::to_string(AZNFSCLIENT_VERSION_MINOR) + "." +
+                              std::to_string(AZNFSCLIENT_VERSION_PATCH) + "]";
     extra_options = std::string("-oallow_other,default_permissions,fsname=") + mount_source;
 
     if (fuse_opt_add_arg(&args, extra_options.c_str()) == -1) {
